3b8f8f1a239c27eedabf330f4256c678e59adbfa
RoyDMAP <roy.dimapilis@yahoo.com>
2025-09-29 17:36:45 -0700

    Initial Commit

---
+++ OrientationVisualizer/OrientationVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationVisualizerApp.swift
+//  OrientationVisualizer
+//
+//  Created by Roy Dimapilis on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct OrientationVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
